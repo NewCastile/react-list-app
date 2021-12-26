21103c800c1669e19db517f2da99b986fea6dca7
New Castile <carlosaguanipa1@gmail.com>
2021-12-26 14:48:25 -0400

    Changed port on url

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
-import React from 'react'
+/** @format */
+
+import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import List from './components/List'
-import Profile from './components/Profile'
-import Form from './components/Form'
+import List from "./components/List"
+import Profile from "./components/Profile"
+import Form from "./components/Form"
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <List tweets="http://localhost:5000/tweets" type="feed"/>
-        </Route>
-        <Route path="/profiles/:user">
-          <Profile />
-        </Route>
-        <Route path="/coment">
-          <Form />
-        </Route>
-      </Switch>
-    </Router>
-  )
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					<List tweets='http://localhost:5000/tweets' type='feed' />
+				</Route>
+				<Route path='/profiles/:user'>
+					<Profile />
+				</Route>
+				<Route path='/coment'>
+					<Form />
+				</Route>
+			</Switch>
+		</Router>
+	)
 }
 
 export { App }
